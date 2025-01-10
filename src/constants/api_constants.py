418a74161ce93d745c421b134c0482d1123c9e24
Gajendran10 <gajendran.mani10@gmail.com>
2025-01-10 22:28:33 +0530

    Test Added

---
+++ src/constants/api_constants.py
@@ -0,0 +1,13 @@
+# APIConstants- Class which contains all the endpoints
+#Keep all the urls
+
+
+class APIConstants:
+    def base_url(self):
+        return "https://restful-booker.herokuapp.com"
+
+    def url_create_booking(self):
+        return "https://restful-booker.herokuapp.com/booking"
+
+    def url_create_token(self):
+        return "https://restful-booker.herokuapp.com/auth"
